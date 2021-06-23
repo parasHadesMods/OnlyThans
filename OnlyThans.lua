454a11c70e3e456358b960fe0ab3b0ff41c836aa
paradigmsort <paradigmsort+hades@gmail.com>
2021-06-23 08:53:31 -0700

    working for trivial rooms

--- OnlyThans.lua
+++ OnlyThans.lua
@@ -8,3 +8,28 @@ local config = {
 if ModConfigMenu then
   ModConfigMenu.Register(config)
 end
+
+local biomeEncounters = {
+  Tartarus = EncounterData.ThanatosTartarus,
+  Asphodel = EncounterData.ThanatosAsphodel,
+  Elysium = EncounterData.ThanatosElysium
+}
+
+ModUtil.WrapBaseFunction("ChooseEncounter", function(baseFunc, currentRun, room)
+  local encounterData = nil
+  for biome, encounter in pairs(biomeEncounters) do
+    if room.RoomSetName == biome then
+      encounterData = encounter
+      break
+    end
+  end
+  
+  if encounterData ~= nil then
+    local encounter = SetupEncounter(encounterData, room)
+
+    return encounter
+  else
+    print("Can't force thanatos in " .. room.Name)
+    return baseFunc(currentRun, currentRoom)
+  end
+end)
